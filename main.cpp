2e2d3497ada4dd62d538b65204742fa10fe0f7cf
FaitelsonAA <gmail@example.com>
2024-07-02 20:59:49 +0300

    code: added output of a-b

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main(int argc, char *argv[]) {
   cout << "Enter A and B: ";
   int a, b;
   cin >> a >> b;
-  cout << a + b;
+
+  cout << "A + B = " << a + b << '\n' << "A - B = " << a - b << '\n';
   return 0;
 }
